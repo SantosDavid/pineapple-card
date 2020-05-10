38be17276d3d38ce608783560ec4932784f98867
SantosDavid <daviddsantosd@gmail.com>
2020-05-10 18:45:35 -0300

    integration with dingo api

--- src/Infrastructure/UI/Laravel/app/Http/Controllers/Api/V1/AuthController.php
+++ src/Infrastructure/UI/Laravel/app/Http/Controllers/Api/V1/AuthController.php
@@ -3,77 +3,21 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use Dingo\Api\Routing\Helpers;
 
 class AuthController extends Controller
 {
-    /**
-     * Create a new AuthController instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth:api', ['except' => ['login']]);
-    }
+    use Helpers;
 
-    /**
-     * Get a JWT via given credentials.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
     public function login()
     {
         $credentials = request(['email', 'password']);
 
-        if (! $token = auth()->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+        if (!$token = auth()->attempt($credentials)) {
+            $this->response->errorUnauthorized();
         }
 
-        return $this->respondWithToken($token);
-    }
-
-    /**
-     * Get the authenticated User.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function me()
-    {
-        return response()->json(auth()->user());
-    }
-
-    /**
-     * Log the user out (Invalidate the token).
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function logout()
-    {
-        auth()->logout();
-
-        return response()->json(['message' => 'Successfully logged out']);
-    }
-
-    /**
-     * Refresh a token.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function refresh()
-    {
-        return $this->respondWithToken(auth()->refresh());
-    }
-
-    /**
-     * Get the token array structure.
-     *
-     * @param  string $token
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    protected function respondWithToken($token)
-    {
-        return response()->json([
+        return $this->response->array([
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60
--- src/Infrastructure/UI/Laravel/routes/api.php
+++ src/Infrastructure/UI/Laravel/routes/api.php
@@ -1,21 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', function ($api) {
-    $api->post('sign-in', 'App\Http\Controllers\Api\V1\CustomerController@store');
-});
+    $api->post('sign-up', 'App\Http\Controllers\Api\V1\CustomerController@store');
 
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'auth'
-], function ($router) {
+    $api->post('login', 'App\Http\Controllers\Api\V1\AuthController@login');
 
-    Route::post('login', 'Api\V1\AuthController@login');
-    Route::post('logout', 'AuthController@logout');
-    Route::post('refresh', 'AuthController@refresh');
-    Route::post('me', 'Api\V1\AuthController@me');
-
+    $api->group(['middleware' => 'api.auth'], function ($api) {
+        // authenticated routes
+    });
 });
