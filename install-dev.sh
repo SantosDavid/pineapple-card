ebd3472c5b9a7493b7833dffb469f127e211b3af
SantosDavid <daviddsantosd@gmail.com>
2020-05-09 23:28:24 -0300

    adding sh to run migrations easier

--- install-dev.sh
+++ install-dev.sh
@@ -5,5 +5,3 @@ docker exec -it pineapple_app composer install
 docker exec -it pineapple_app php artisan key:generate
 docker exec -it pineapple_server chown www-data src/Infrastructure/UI/Laravel/storage/ -R
 docker exec -it pineapple_server chown www-data src/Infrastructure/UI/Laravel/bootstrap/ -R
-
-
