cef7e2f6fc8e944b08bb16e9962849ec5c26cc11
SantosDavid <daviddsantosd@gmail.com>
2020-05-13 01:14:59 -0300

    adjusting install dev

--- install-dev.sh
+++ install-dev.sh
@@ -3,6 +3,7 @@ cp .env.example .env
 cp src/Infrastructure/UI/Laravel/.env.example src/Infrastructure/UI/Laravel/.env
 docker-compose up -d --build
 docker exec -it pineapple_app composer install
+docker exec -it pineapple_app mkdir src/Infrastructure/UI/Laravel/bootstrap/cache
 docker exec -it pineapple_app php artisan key:generate
 docker exec -it pineapple_server chown www-data src/Infrastructure/UI/Laravel/storage/ -R
 docker exec -it pineapple_server chown www-data src/Infrastructure/UI/Laravel/bootstrap/ -R
